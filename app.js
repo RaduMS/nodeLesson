ff196227380bcf890ecd51590d541f5e2f2bf699
RaduSebastian <g.cont.sebi@gmail.com>
2019-12-22 12:54:26 +0200

    - use ghit ignore - use express for GET

--- app.js
+++ app.js
@@ -35,3 +35,54 @@ console.log("ceva");
 // logger.log('asta a fost logat de logger');
 
 // ================= Buidl APIs whit Node and Express =======================
+
+const express = require ('express');
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3001;
+var courses = [
+    {id:1, name: "math"},
+    {id:2, name: "english"},
+    {id:3, name: "german"}
+];
+
+app.get('/', function (req, res) {
+    res.send('hellow world');
+});
+
+app.get('/api/courses', function (req, res) {
+    res.send(courses);
+});
+
+app.post('api/courses', function (req, res) {
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/courses/:id', function (req, res) {
+    var course = courses.find( elem => elem.id === parseInt(req.params.id));
+    if (!course){
+        res.status(404).send('no course whit id: ' + parseInt(req.params.id));
+    } else {
+        res.send(course);
+    }
+});
+
+
+app.get('/api/posts/:year/:month', function (req, res) {
+    res.send(req.params);
+    res.send(req.query);
+})
+
+app.listen(port , function () {
+   console.log(`Listening port: ${port}`);
+});
+
+
+
+
+
